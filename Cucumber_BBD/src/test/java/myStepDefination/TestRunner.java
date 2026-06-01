c4efe1815a2789efe1d261cf5fc465df6e02fd5b
Umesh Das <dasumesh2002@gmail.com>
2026-06-01 14:49:24 +0530

    cucumber commit

--- Cucumber_BBD/src/test/java/myStepDefination/TestRunner.java
+++ Cucumber_BBD/src/test/java/myStepDefination/TestRunner.java
@@ -1,18 +1,22 @@
 package myStepDefination;
 
 import org.junit.runner.RunWith;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-//@SuppressWarnings("deprecation")
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/myFeatures", 
-glue = {"myStepDefination"}, 
-tags = "@Regression",
-plugin = {"pretty", "html:target/htmlreports/report.html",
-		             "json:target/htmlreports/report",
-		             "junit:target/htmlreports/report"})
+@CucumberOptions(
+    features = "src/test/resources/myFeatures",
+    glue = {"myStepDefination"},
+    tags = "@Regression",
+    plugin = {
+        "pretty",
+        "html:target/htmlreports/report.html",
+        "json:target/htmlreports/report.json",
+        "junit:target/htmlreports/report.xml"
+    }
+)
+
 public class TestRunner {
 
 }
