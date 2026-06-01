63da8030c7969826d39d3f9272f127349ad25ba9
Umesh Das <dasumesh2002@gmail.com>
2026-06-01 16:09:40 +0530

    tag update

--- Cucumber_BBD/src/test/java/myStepDefination/TestRunner.java
+++ Cucumber_BBD/src/test/java/myStepDefination/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
     features = "src/test/resources/myFeatures",
     glue = {"myStepDefination"},
-    tags = "@Regression",
+//    tags = "@Regression",
     plugin = {
         "pretty",
         "html:target/htmlreports/report.html",
